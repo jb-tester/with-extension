751f121e2fbb9eefc4e864661af67e0712ad3907
irina <irina@me.com>
2019-07-29 17:59:00 +0300

    add dummy class

---
+++ src/main/java/a/b/c/Foo.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by irina on 7/29/2019.
+ * Project: with-extension
+ * *******************************
+ */
+public class Foo {
+}
